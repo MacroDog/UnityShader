be0d5f2907b841dcbef55b0f8805d7d3c7fc1ccf
MacroDog <yifeng_email@126.com>
2018-01-17 22:17:58 +0800

    add file

---
+++ Assets/Script/My_PostEffectsBase.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class My_PostEffectsBase : MonoBehaviour {
+
+    protected void CheckResources()
+    {
+        bool isSupported = CheckSupport();
+    }
+	protected bool CheckSupport()
+    {
+        if (SystemInfo.supportsImageEffects==false)
+        {
+            Debug.LogWarning("This platform does not support image effect or render texture");
+            return false;
+        }
+        return true;
+    }
+    protected void NotSupport()
+    {
+        enabled = false;
+    }
+    protected void Start()
+    {
+        CheckResources();
+    }
+
+    protected Material CheckShaderAndCreateMaterial(Shader shader,Material material)
+    {
+        if (shader == null)
+        {
+            return null;
+        }
+        if (shader.isSupported&&material!=null&&material.shader == shader)
+        {
+            return material;
+        }
+        if (!shader.isSupported)
+        {
+            return null;
+        }
+        else
+        {
+            material = new Material(shader);
+            material.hideFlags = HideFlags.DontSave;
+            if (material)
+            {
+                return material;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
