48995d297e5e4594164137b8dbaf10953d3606ba
yifengxie <yifeng.macrodog@gmail.com>
2018-02-01 18:40:17 +0800

    add fogWithDepthTexture

---
+++ Assets/Scripts_My/My_FogWithDepthTexture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class My_FogWithDepthTexture : My_PostEffectsBase {
+
+    public Shader FogWithDepthTextureShader;
+
+    private Material FogMaterial = null;
+
+    private Material material {
+        get {
+            FogMaterial = CheckShaderAndCreateMaterial(FogWithDepthTextureShader, FogMaterial);
+            return FogMaterial;
+        } 
+    }
+
+    public Transform transform;
+    public Color FogColor;
+    public float Denisty = 1.0f;
+    public float Start;
+    public float End;
+
+    void OnRenderImage(RenderTexture sources, RenderTexture destination) {
+        
+    }
+}
